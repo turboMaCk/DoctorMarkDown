50418de549fe8bf93b8cb1f16f21ac0ee91dcc98
Marek Fajkus <marek.faj@gmail.com>
2016-07-23 15:06:45 +0300

    Test flat structure

--- spec/filesystem/browser-spec.ts
+++ spec/filesystem/browser-spec.ts
@@ -1,18 +1,20 @@
 ///<reference path="../../header_files/jasmine.d.ts"/>
 import Browser from '../../lib/filesystem/browser';
 
-export default describe('file system browser (tree parser)', () => {
-    const settings = {
-        files: ['a.md', 'b.md'],
-        ignore: ['ignore']
-    }
+export default describe('file browser util', () => {
 
-    const browser = Browser(settings);
+    const root = './spec/mock/fs';
 
-    describe('parse mocks correctly', () => {
-        const root = './spec/mock/fs';
-        let error, result, runs;
+    describe('file system browser (tree parser)', () => {
+        const settings = {
+            recursive: true,
+            files: ['a.md', 'b.md'],
+            ignore: ['ignore']
+        };
 
+        const browser = Browser(settings);
+
+        let error, result;
         beforeEach((done) => {
             browser(root, (err, res) => {
                 error = err;
@@ -23,28 +25,64 @@ export default describe('file system browser (tree parser)', () => {
 
         it('do not return err', () => expect(error).toBe(null));
 
-        it('return correct structure', () => {
-            const expected = {
-                path: `${root}`,
-                children: [
-                    { path: `${root}/a.md` },
-                    { path: `${root}/deep`, children: [
-                        { path: `${root}/deep/a.md` },
-                        { path: `${root}/deep/b.md` },
-                    ] },
-                    { path: `${root}/skipped/deeper`, children: [
-                        { path: `${root}/skipped/deeper/a.md` }
-                    ] },
-                    { path: `${root}/deepskip/skipped/deepest`, children: [
-                        { path: `${root}/deepskip/skipped/deepest/b.md` }
-                    ] }
-                ]
-            };
+        describe('parse mocks correctly', () => {
+            it('return correct structure', () => {
+                const expected = {
+                    path: `${root}`,
+                    children: [
+                        { path: `${root}/a.md` },
+                        { path: `${root}/deep`, children: [
+                            { path: `${root}/deep/a.md` },
+                            { path: `${root}/deep/b.md` },
+                        ] },
+                        { path: `${root}/skipped/deeper`, children: [
+                            { path: `${root}/skipped/deeper/a.md` }
+                        ] },
+                        { path: `${root}/deepskip/skipped/deepest`, children: [
+                            { path: `${root}/deepskip/skipped/deepest/b.md` }
+                        ] }
+                    ]
+                };
 
-            expect(result.path).toEqual(root);
-            expect(result.children[0].path).toEqual(`${root}/a.md`);
-            expect(result.children[3].path).toEqual(`${root}/deepskip/skipped/deepest`);
-            expect(result).toEqual(expected);
+                expect(result.path).toEqual(root);
+                expect(result.children[0].path).toEqual(`${root}/a.md`);
+                expect(result.children[3].path).toEqual(`${root}/deepskip/skipped/deepest`);
+                expect(result).toEqual(expected);
+            });
+        });
+    });
+
+    describe('flat file system browse', () => {
+        const settings = {
+            recursive: false,
+            files: ['a.md', 'b.md'],
+            ignore: ['ignore']
+        };
+
+        const browser = Browser(settings);
+
+        let error, result;
+        beforeEach((done) => {
+            browser(root, (err, res) => {
+                error = err;
+                result = res;
+                done();
+            });
+        });
+
+        it('do not return err', () => expect(error).toBe(null));
+
+        describe('parse mock flattly', () => {
+            it ('return correct structure', () => {
+                const expected = {
+                    path: `${root}`,
+                    children: [
+                        { path: `${root}/a.md` }
+                    ]
+                };
+
+                expect(result).toEqual(expected);
+            });
         });
     });
 });
